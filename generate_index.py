37a2ca67a4165dcd6e9902f4bcf5f76e51c3261d
gglck99 <66121115+gglck99@users.noreply.github.com>
2020-12-02 21:07:43 +0700

    Add files via upload

---
+++ generate_index.py
@@ -0,0 +1,30 @@
+#coding: utf-8
+
+from horoscope import generate_prophecies
+from datetime import datetime as dt
+
+def generate_head(title):
+    head = "<title>" + title + "</title>"
+    head += '<meta charset="utf-8">'
+    return "<head>" + head + "</head>"
+
+def generate_body(header, paragraphs: list):
+    body = "<h1>" + header + "</h1>"
+    for p in paragraphs:
+        body += "<p>" + p + "</p>"
+    return "<body>" + body + "<body>"
+
+def save_page():
+    prophecies = generate_prophecies()
+    today = dt.now().date()
+    body = generate_body(header= "Гороскоп на " + str(today), paragraphs=prophecies)
+    head = generate_head(title= "Гороскоп")
+    page = head + body
+
+
+
+    fp = open("in.html", "w")
+    print(page, file=fp)
+    fp.close()
+
+save_page()
